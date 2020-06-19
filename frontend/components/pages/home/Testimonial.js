7530a8fdaba6587f041feaee994d33fa6395bea8
JoachimBrasier <jo-brasier@outlook.com>
2020-06-19 15:21:27 +0200

    Add testimonial

--- frontend/components/pages/home/Testimonial.js
+++ frontend/components/pages/home/Testimonial.js
@@ -1,8 +1,57 @@
 import React from "react";
+import {
+  Grid,
+  Typography,
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  makeStyles,
+} from "@material-ui/core";
+import Rating from "@material-ui/lab/Rating";
 
-export default ({ data }) =>
-  data.map((testimonial) => (
-    <div key={testimonial._id}>
-      <h3>{testimonial.author}</h3>
-    </div>
-  ));
+const useStyles = makeStyles((theme) => ({
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+}));
+
+export default ({ data }) => {
+  const classes = useStyles();
+
+  return (
+    <section>
+      <Typography variant="h5" align="center" style={{ padding: "20px 0" }}>
+        Témoignages
+      </Typography>
+      <Grid container spacing={3}>
+        {data.map((testimonial) => (
+          <Grid item key={testimonial._id} xs={12} sm={6} md={4}>
+            <Card style={{ height: "100%" }}>
+              <CardHeader
+                avatar={
+                  <Avatar
+                    src={`http://localhost:1337${testimonial.image.url}`}
+                    variant="square"
+                    imgProps={{ style: { objectFit: "contain" } }}
+                    className={classes.large}
+                  />
+                }
+                title={testimonial.author}
+                subheader={
+                  <Rating value={testimonial.rate} readOnly size="small" />
+                }
+              />
+              <CardContent style={{ paddingTop: 0 }}>
+                <Typography variant="body2" color="textSecondary">
+                  {testimonial.comment}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </section>
+  );
+};
