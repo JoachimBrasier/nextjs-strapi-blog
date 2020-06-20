0ff9383cf1cde83550f95f044887a122c22af05d
JoachimBrasier <jo-brasier@outlook.com>
2020-06-20 17:55:28 +0200

    Add applicant list

---
+++ frontend/components/pages/home/Applicants.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Link from "next/link";
+import {
+  Typography,
+  Grid,
+  Card,
+  CardMedia,
+  CardActions,
+  CardContent,
+  Button,
+} from "@material-ui/core";
+
+export default ({ data }) => {
+  return (
+    <section>
+      <Typography variant="h5" align="center" style={{ padding: "20px 0" }}>
+        Candidats
+      </Typography>
+      <Grid container spacing={3}>
+        {data.map((applicant) => (
+          <Grid item key={applicant._id} xs={12} sm={4} md={3}>
+            <Card>
+              <CardMedia
+                image={`http://localhost:1337${applicant.picture.url}`}
+                style={{ height: 200 }}
+              />
+              <CardContent>
+                <Typography variant="h6">{applicant.full_name}</Typography>
+                <Typography
+                  variant="subtitle1"
+                  color="textSecondary"
+                  gutterBottom
+                  style={{ fontWeight: "bold" }}
+                >
+                  {applicant.training.job_title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {applicant.description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Link href={`/candidat/${applicant._id}`}>
+                  <Button variant="contained" color="primary" fullWidth>
+                    En savoir plus
+                  </Button>
+                </Link>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </section>
+  );
+};
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import fetch from "../lib/fetch";
+import Applicants from "../components/pages/home/Applicants";
 import Testimonial from "../components/pages/home/Testimonial";
 
-const Home = ({ testimonials }) => (
+const Home = ({ applicants, testimonials }) => (
   <>
+    <Applicants data={applicants} />
     <Testimonial data={testimonials} />
   </>
 );
 
 Home.getInitialProps = async () => {
+  const applicants = await fetch("get", "applicants");
   const testimonials = await fetch("get", "testimonials?_limit=3");
-  return { testimonials };
+
+  return { applicants, testimonials };
 };
 
 export default Home;
