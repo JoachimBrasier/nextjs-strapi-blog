85bd06e17da48ca19746d8fd85485e439d63091d
JoachimBrasier <jo-brasier@outlook.com>
2020-06-21 19:55:52 +0200

    Add date sort

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -11,8 +11,14 @@ const Home = ({ applicants, testimonials }) => (
 );
 
 Home.getInitialProps = async () => {
-  const applicants = await fetch("get", "applicants?_limit=4");
-  const testimonials = await fetch("get", "testimonials?_limit=3");
+  const applicants = await fetch(
+    "get",
+    "applicants?_limit=4&_sort=createdAt:desc"
+  );
+  const testimonials = await fetch(
+    "get",
+    "testimonials?_limit=3&_sort=createdAt:desc"
+  );
 
   return { applicants, testimonials };
 };
